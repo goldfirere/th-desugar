cd382e016339cd5d5dbe55edf78a1027020736c0
Mathieu Boespflug <m@tweag.io>
2014-11-25 13:34:37 +0100

    Don't test phantom type params on GHC < 7.8.

--- t/Foo.hs
+++ t/Foo.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Foo where
 
 import Language.Haskell.TH.Lift
 
+-- Phantom type parameters can't be dealt with poperly on GHC < 7.8.
+#if MIN_VERSION_template_haskell(2,9,0)
 data (Eq a) => Foo a b = Foo a Char | Bar a
+#else
+data (Eq a) => Foo a = Foo a Char | Bar a
+#endif
     deriving Show
 
 newtype Rec a = Rec { field :: a }
